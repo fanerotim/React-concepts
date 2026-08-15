0bb3ddecbae699250efe53f3c229430e85637369
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-08-15 17:43:38 +0300

    Add functionality to control color mode

--- Override native back button on mobile/Override behavior of native back button on mobile/src/App.jsx
+++ Override native back button on mobile/Override behavior of native back button on mobile/src/App.jsx
@@ -1,9 +1,13 @@
 import ProductList from "./components/ProductList"
+import { ColorModeButton, useColorMode } from "./components/ui/color-mode"
 
 function App() {
 
+  const {toggleColorMode} = useColorMode();
+
   return (
     <>
+      <ColorModeButton onClick={toggleColorMode}/>
       <ProductList />
     </>
   )
