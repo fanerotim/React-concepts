ea56a51db42061a9d946aaebccb23262701a6853
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-08-16 18:32:00 +0300

    config tanstack query provider

--- Override native back button on mobile/Override behavior of native back button on mobile/src/main.jsx
+++ Override native back button on mobile/Override behavior of native back button on mobile/src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from './components/ui/provider.jsx'
 import App from './App.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider>
-      <App />
+      <QueryClientProvider client={new QueryClient()}>
+        <App />
+      </QueryClientProvider>
     </Provider>
   </StrictMode>,
 )
