f2a6cda43ad947a3fcbe78f2ff5ccf528f3ee9f2
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-08-21 22:06:32 +0300

    change Departments component to use Chakra's Select component instead of NativeSelect - not yet fully implemented; A bug left to clear, but rendered at the moment with dummy data

--- Override native back button on mobile/Override behavior of native back button on mobile/src/components/Departments.jsx
+++ Override native back button on mobile/Override behavior of native back button on mobile/src/components/Departments.jsx
@@ -1,46 +1,75 @@
-import { NativeSelect, Button, Flex } from "@chakra-ui/react"
+import { Select, Button, Flex, Portal, createListCollection } from "@chakra-ui/react"
 import { departments } from "../api/departments";
 import { useState } from "react";
 
-const Departments = ({register, onSubmit}) => {
-    const [_, setCurDept] = useState(null);
+const Departments = ({ register, onSubmit, Controller, control }) => {
+    const [dept, setCurDept] = useState(null);
 
     return (
         <form onSubmit={onSubmit}>
-            <Flex 
-                direction={"column"} 
-                alignItems={"center"} 
-                width={"fit-content"} 
+            <Flex
+                direction={"column"}
+                alignItems={"center"}
+                width={"fit-content"}
                 margin={"2rem auto"}
                 gap={"2"}
             >
-                <NativeSelect.Root width={"sm"}>
-                    <NativeSelect.Field
-                        {...register("department")}
-                        onChange={(e) => setCurDept(e.currentTarget.value)}
-                        defaultValue={'Select an option'}
-                    >
-                        {departments.departments.map(({ departmentId, displayName }) => (
-                            <option
-                                key={departmentId}
-                                value={departmentId}
-                            >
-                                {displayName}
-                            </option>
-                        ))}
-                    </NativeSelect.Field>
-                    <NativeSelect.Indicator />
-                </NativeSelect.Root>
+                <Controller
+                    control={control}
+                    name="department"
+                    render={({ field }) => (
+                        <Select.Root
+                            name={field.name}
+                            value={field.value}
+                            onValueChange={({value}) => field.onChange(value)}
+                            w={"xs"}
+                            collection={frameworks}
+                        >
+                            <Select.Label>
+                                Choose a department
+                            </Select.Label>
+                            <Select.Control>
+                                <Select.Trigger>
+                                    <Select.ValueText placeholder="Select department" onChange={() => console.log('change detected')} />
+                                    <Select.Indicator />
+                                </Select.Trigger>
+                            </Select.Control>
+                            <Portal>
+                                <Select.Positioner>
+                                    <Select.Content>
+                                        {frameworks.items.map((framework) => (
+                                            <Select.Item
+                                                key={framework.value}
+                                                item={framework}
+                                            >
+                                                {framework.label}
+                                            </Select.Item>
+                                        ))}
+                                    </Select.Content>
+                                </Select.Positioner>
+                            </Portal>
+                        </Select.Root>)
+                    }
+                />
                 <Button
                     size={"xs"}
-                    type="submit"
                     alignSelf={"end"}
                 >
                     Submit
                 </Button>
             </Flex>
-        </form>
+        </form >
     )
 }
+
+
+const frameworks = createListCollection({
+  items: [
+    { label: "React.js", value: "react" },
+    { label: "Vue.js", value: "vue" },
+    { label: "Angular", value: "angular" },
+    { label: "Svelte", value: "svelte" },
+  ],
+})
 
 export default Departments;
