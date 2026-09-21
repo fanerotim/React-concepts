93c5bfbf5a46227db8f5c873890e72822ae2eed3
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-09-21 22:25:43 +0300

    Add some styles to product card component

--- Override native back button on mobile/Override behavior of native back button on mobile/src/components/ProductCard.jsx
+++ Override native back button on mobile/Override behavior of native back button on mobile/src/components/ProductCard.jsx
@@ -7,18 +7,25 @@ const ProductCard = ({ data }) => {
         return
     }
 
+    const DESCRIPTION_LENGTH = 100;
+    const TITLE_LENGTH = 60;
+
     return (
         <Card.Root
             size="sm"
             width={"xs"}
             margin={"0 auto"}
+            variant={'subtle'}
+            colorPalette={'red'}
         >
             <Card.Header>
                 <Card.Title
                     fontWeight={"medium"}
                     textStyle={"lg"}
                 >
-                    {data?.title}
+                    {data?.title.length > TITLE_LENGTH
+                        ? data.title.slice(0, TITLE_LENGTH) + '...'
+                        : data.title}
                 </Card.Title>
 
                 {data?.department &&
@@ -42,21 +49,28 @@ const ProductCard = ({ data }) => {
 
             >
                 <Image
+                    height={'80%'}
                     objectFit={"cover"}
                     alt={data?.title}
                     src={data?.primaryImageSmall ?? data?.primaryImage}
                 >
                 </Image>
                 <Card.Description>
-                    {data?.medium}
+                    {data?.medium.length > DESCRIPTION_LENGTH 
+                        ? `${data?.medium.slice(0, DESCRIPTION_LENGTH)}...` 
+                        : data?.medium}
                 </Card.Description>
             </Card.Body>
 
             <Card.Footer>
                 <Group>
                     <Button
-                        size={"sm"}
                         variant={"subtle"}
+                        size={"xs"}
+                        bg={'yellow.200'}
+                        color={'black'}
+                        fontWeight={'normal'}
+                        _hover={{bg: 'red.400'}}
                     >
                         Details
                     </Button>
@@ -64,6 +78,10 @@ const ProductCard = ({ data }) => {
                     <Button
                         asChild
                         size={"xs"}
+                        bg={'orange.100'}
+                        color={'black'}
+                        fontWeight={'normal'}
+                        _hover={{bg: 'red.400'}}
                     >
                         <a
                             href={data?.objectURL}
